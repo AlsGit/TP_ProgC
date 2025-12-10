990d77914c67833e7f036e6a82422dcf77fcc61e
AlsGit <manathan.ramanantsialonina@laposte.net>
2025-12-10 17:43:43 +0100

    Ajout de commentaires pour expliquer mes choix + initiation à make pour les prochains exercices

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <math.h> 
+/* math.h contient la variable M_PI pour PI
+ * NB : ajouter l'option -lm à gcc pour le lien avec math.h
+ * Exemple : gcc -o cercle cercle.c -lm
+ */
 
 int main() {
-    //double pi = 3.1416;
-    double r = 6;  
-    printf("L'air du cercle = %f\n", M_PI*(r*r));
-    printf("Le périmètre du cercle = %f\n", M_PI*(r+r));
+	double rayon = 6;
+    	double perimetre = M_PI * rayon * 2; // utilisation de M_PI dans un calcul 
+	double aire = M_PI * pow(rayon,2); // pow(base,puissance) vient de math.h 
+
+	printf("Rayon du cercle : %.2f\n", rayon);
+	printf("Aire du cercle = %.2f\n", aire);
+    	printf("Périmètre du cercle = %.2f\n", perimetre);
+	// %.2f affiche une variable de type float limité à 2 décimales
 }
 
+/* Note à moi-même pour la suite du cours
+ * Il est possible d'automatiser un peu la compilation grâce à make
+ * $ touch makefile # dans le répertoire où se trouve le fichier FICHIER.c à compiler
+ * 
+ * makefile:
+ * 	CC = gcc
+ *	CFLAGS = -Wall -Wextra -lm # noté l'option -lm pour le lien math.h :)
+ *	%: 
+ *	
+ * La ligne "%:" prend les variables CC et CFLAGS et génère la commande suivante :
+ * $ ${CC} ${FLAGS} -o FICHIER -c FICHIER.c
+ * 	
+ * Ce que le terminal traduira par : 
+ * $ gcc -Wall -Wextra -lm -o FICHIER -c FICHIER.c
+ *
+ * Exemple utilisation pour cet exercice : 
+ * $ make cercle # noté l'absence de l'extension .c pour make
+ */
