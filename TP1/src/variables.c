1f011515246999fcf211420842dd527609671366
AlsGit <manathan.ramanantsialonina@laposte.net>
2025-12-10 18:48:54 +0100

    correction de l'affichage de la valeur de uShort

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
 int main() {
-  char myChar = 'a';
-  unsigned char uChar = 212;
-  short myShort = 12;
-  unsigned short uShort = 12;
-  int myInt = 12;
-  unsigned int uInt = 42;
-  long int lInt = 123456L;
-  unsigned long int ulInt = 123456UL;
-  long long int llInt = 123456789LL;
-  unsigned long long int ullInt = 123456789LL;
-  float myFloat = 1234.56;
-  double myDouble = 3.14159;
-  long double lDouble = 10.15L;
+	// définition des variables avec des valeurs de leur base
+	char myChar = 'a';
+	unsigned char uChar = 212;
+	short myShort = 12;
+	unsigned short uShort = 12;
+	int myInt = 12;
+	unsigned int uInt = 42;
+	long int lInt = 123456L;
+	unsigned long int ulInt = 123456UL;
+	long long int llInt = 123456789LL;
+	unsigned long long int ullInt = 123456789LL;
+	float myFloat = 1234.56;
+	double myDouble = 3.14159;
+	long double lDouble = 10.15L;
 
-  printf("myChar = %c\n", myChar);
-  printf("uChar = %hhu\n", uChar);
-  printf("myShort = %hd\n", myShort);
-  printf("uShort = %hd\n", myShort);
-  printf("myInt = %i\n", myInt);
-  printf("uInt = %u\n", uInt);
-  printf("lInt = %ld\n", lInt);
-  printf("ulInt = %lu\n", ulInt);
-  printf("llInt = %lld\n", llInt);
-  printf("ullInt = %llu\n", ullInt);
-  printf("myFloat = %f\n", myFloat);
-  printf("myDouble = %lf\n", myDouble);
-  printf("lDouble = %Lf\n", lDouble);
-  
-  return 0;
+	// affichage des valeurs des variables définies
+	printf("myChar = %c\n", myChar);
+	printf("uChar = %hhu\n", uChar);
+	printf("myShort = %hd\n", myShort);
+	printf("uShort = %hu\n", uShort);
+	printf("myInt = %i\n", myInt);
+	printf("uInt = %u\n", uInt);
+	printf("lInt = %ld\n", lInt);
+	printf("ulInt = %lu\n", ulInt);
+	printf("llInt = %lld\n", llInt);
+	printf("ullInt = %llu\n", ullInt);
+	printf("myFloat = %f\n", myFloat);
+	printf("myDouble = %lf\n", myDouble);
+	printf("lDouble = %Lf\n", lDouble);
 }
